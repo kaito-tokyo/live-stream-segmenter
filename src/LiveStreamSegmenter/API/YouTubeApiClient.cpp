9c8aeb37b5ecdd4ef59d110c8880ef43c5a88a75
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-10 14:45:03 +0900

    Update YouTubeApiClient.cpp

--- src/LiveStreamSegmenter/API/YouTubeApiClient.cpp
+++ src/LiveStreamSegmenter/API/YouTubeApiClient.cpp
@@ -106,12 +106,6 @@ inline std::string doGet(const char *url, const std::string &accessToken)
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
 
-	curl_off_t contentLength = 0;
-	curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &contentLength);
-	if (contentLength > 0 && contentLength < static_cast<curl_off_t>(std::numeric_limits<size_t>::max())) {
-		readBuffer.reserve(static_cast<size_t>(contentLength));
-	}
-
 	CURLcode res = curl_easy_perform(curl);
 
 	curl_slist_free_all(headers);
