618fdc5d84c876e43c86e9957fd0f1dfc8f4999b
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-13 21:50:33 +0900

    Unify and update license headers across source files
    Standardized license headers in all source and header files to consistently reference either the MIT License or GPL-3.0-or-later, as appropriate. Updated copyright statements and added references to license files in the distribution root for clarity and compliance.

--- src/LiveStreamSegmenter/UI/SettingsDialog.hpp
+++ src/LiveStreamSegmenter/UI/SettingsDialog.hpp
@@ -1,3 +1,17 @@
+/*
+ * Live Stream Segmenter
+ * Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; for more details see the file 
+ * "LICENSE.GPL-3.0-or-later" in the distribution root.
+ */
+
 #pragma once
 
 #include <QDialog>
--- src/UpdateChecker/UpdateChecker.hpp
+++ src/UpdateChecker/UpdateChecker.hpp
@@ -1,20 +1,11 @@
 /*
-Live Background Removal Lite
-Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * UpdateChecker
+ * Copyright (c) 2025 Kaito Udagawa umireon@kaito.tokyo
+ *
+ * This software is licensed under the MIT License.
+ * For the full text of the license, see the file "LICENSE.MIT" 
+ * in the distribution root.
+ */
 
 #pragma once
 
