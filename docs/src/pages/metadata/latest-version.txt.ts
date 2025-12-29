5bcc9a0aca9253deb4ea06f17845013134c41056
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-29 16:15:52 +0900

    Update latest-version.txt.ts

--- docs/src/pages/metadata/latest-version.txt.ts
+++ docs/src/pages/metadata/latest-version.txt.ts
@@ -1,16 +1,5 @@
-import { Octokit } from "@octokit/rest";
-
-import { GITHUB_OWNER, GITHUB_REPO } from "../../lib/info.js";
-
 export async function GET() {
-  const octokit = new Octokit({ auth: import.meta.env.GITHUB_TOKEN });
-
-  const latestRelease = await octokit.rest.repos.getLatestRelease({
-    owner: GITHUB_OWNER,
-    repo: GITHUB_REPO,
-  });
-
-  return new Response(latestRelease.data.tag_name, {
+  return new Response("0.1.0", {
     status: 200,
     headers: {
       "Content-Type": "text/plain",
