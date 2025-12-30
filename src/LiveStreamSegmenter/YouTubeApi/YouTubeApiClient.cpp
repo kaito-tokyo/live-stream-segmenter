3116696b8ca8b3d1419469cdbf09c5b541e89e0c
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-31 02:18:01 +0900

    Update YouTubeApiClient.cpp

--- src/LiveStreamSegmenter/YouTubeApi/YouTubeApiClient.cpp
+++ src/LiveStreamSegmenter/YouTubeApi/YouTubeApiClient.cpp
@@ -201,9 +201,8 @@ std::string getLowercaseExtension(const std::filesystem::path &p)
 {
 	std::string ext = p.extension().string();
 
-	std::transform(ext.begin(), ext.end(), ext.begin(), [](char c) {
-		return static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
-	});
+	std::transform(ext.begin(), ext.end(), ext.begin(),
+		       [](char c) { return static_cast<char>(std::tolower(static_cast<unsigned char>(c))); });
 
 	return ext;
 }
