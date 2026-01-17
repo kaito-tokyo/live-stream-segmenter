1c0fbb2864cda6d346fc52acd4db664cadfe595f
Kaito Udagawa <umireon@kaito.tokyo>
2026-01-17 12:06:42 +0900

    Remove redundant fetchStreamKeys call in settings dialog
    The fetchStreamKeys() method was called before showing the settings dialog, but is no longer needed at this location. This streamlines the dialog initialization process.

--- src/LiveStreamSegmenter/UI/StreamSegmenterDock.cpp
+++ src/LiveStreamSegmenter/UI/StreamSegmenterDock.cpp
@@ -523,7 +523,6 @@ void StreamSegmenterDock::onSettingsButtonClicked()
 	}();
 
 	SettingsDialog settingsDialog(runtime_, authStore_, eventHandlerStore_, youTubeStore_, logger, this);
-	settingsDialog.fetchStreamKeys();
 	settingsDialog.loadLocalStorageData();
 	settingsDialog.exec();
 }
